458da0be1e9e63778bec3afe7f27b4984d53e58a
Matthias Chan <blasterdash@gmail.com>
2024-02-21 18:32:45 +0800

    update search for countries and made website more reponsive

--- index.js
+++ index.js
@@ -6,25 +6,30 @@ const app = express();
 const port = 3000;
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-const API_GEO = "https://get.geojs.io/v1/ip/geo.json"
+const API_KEY = "c2026e50be88d44a0bef30890625912d"
+const API_GEO = "http://api.openweathermap.org/geo/1.0/direct?"
 const API_WEA = "https://api.met.no/weatherapi/locationforecast/2.0/complete?lat="
 
-app.get("/", async (req, res) => {
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+})
+
+app.post("/", async (req, res) => {
     try {
-        const geoResult = await axios.get(API_GEO);
-        const weatherResult = await axios.get(`${API_WEA}${geoResult.data.latitude}&lon=${geoResult.data.longitude}`, {
+        const geoResult = await axios.get(`${API_GEO}q=${req.body.country}&limit=1&appid=${API_KEY}`);
+        const weatherResult = await axios.get(`${API_WEA}${geoResult.data[0].lat}&lon=${geoResult.data[0].lon}`, {
             headers: {'User-agent': "weatherapp"}
         } );
         const weatherResults = weatherResult.data.properties.timeseries[1].data.instant.details;
         res.render("index.ejs", {
-            country: geoResult.data.country,
+            country: geoResult.data[0].name,
             data: weatherResults
         });
     } catch(error){
         console.log(error.response);
-    }
+        res.render("index.ejs");
+    };
 })
-
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
